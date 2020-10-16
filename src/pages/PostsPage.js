9d78b800fdb255ad42a302524145f42317e1360d
sedx876 <swatkins876@gmail.com>
2020-10-16 09:31:51 -0400

    added postpage component

---
+++ src/pages/PostsPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {connect} from 'react-redux'
+
+const PostsPage = () => {
+  return (
+    <section>
+      <h1>Posts</h1>
+    </section>
+  )
+}
+
+export default PostsPage
